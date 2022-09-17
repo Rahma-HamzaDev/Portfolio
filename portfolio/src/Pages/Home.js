277066b3476779139ab5279a10a6245cd7acd3d4
Rahma Hamza <rahmaa.hamza.2001@gmail.com>
2022-09-17 17:18:02 +0200

    add information

--- portfolio/src/Pages/Home.js
+++ portfolio/src/Pages/Home.js
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import webImages from '../Images/Web-Dev-Blog.png'
 function Home() {
   return (
-    <div>Home</div>
+<div class="alert alert-primary" role="alert">
+    <img src="/images/rahmaa.jpg" alt="my-photo" className='avatar' />
+    <h1>Rahma Hamza</h1>
+     <h2 className='text'>Hello My name is Rahma. I am a fullstack developer</h2>
+    <img src={webImages} alt="web-Dev" />
+      </div>
+
   )
 }
 
--- portfolio/src/Pages/Projects.js
+++ portfolio/src/Pages/Projects.js
@@ -1,8 +1,31 @@
 import React from 'react'
-
+const myProjects = [{
+  title: 'business Land',
+  description: 'A simple landing page developed with html and CSS',
+  URL: 'https://rahma-hamzadev.github.io/busniess-land/'
+},
+{
+  title: 'TODOM',
+  description: 'A simple to do list developed with html , CSS and javascript',
+  URL: 'https://todom-devmastery.vercel.app/'
+}
+]
 function Projects() {
   return (
-    <div>Projects</div>
+    <div>
+      {myProjects.map((item, index) =>
+        <div style={{ padding: 20 }}>
+          <div className="card" >
+            <div className="card-body">
+              <h5 className="card-title">{item.title}</h5>
+              <h6 className="card-subtitle mb-2 text-muted"> {item.description}</h6>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href={item.URL} target='-blank' className="card-link">Preview</a>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 
