b0281375873853d84bc816172cebfbdc216e9033
Rahma Hamza <rahmaa.hamza.2001@gmail.com>
2022-09-18 14:54:10 +0200

    add skills

--- portfolio/src/Pages/Skills.js
+++ portfolio/src/Pages/Skills.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import axios from "axios";
 
 function Skills() {
+  const [technologie, settechnologie] = React.useState([])
+  React.useEffect(() => {
+    axios.get('https://devmastery-assets.vercel.app/technologies.json')
+      .then((response) => { settechnologie(response.data) })
+      .catch((error) => { console.log(error) })
+  }, [])
   return (
-    <div>Skills</div>
+    <div className='row'>
+      {technologie.map((item) =>
+      (
+        <div key={item.id} className='col-12 col-sm-6 col-md-4 col-lg-3 p-3' >
+          <div className="card" >
+            <img src={item.image} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">{item.name}</h5>
+              <a href="#" className="btn btn-primary">learn about</a>
+            </div>
+          </div>
+        </div>
+      )
+      )}
+    </div>
+
   )
 }
 
